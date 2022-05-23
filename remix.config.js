82103dcb83eb3e0311e59d2d2a80b39fe5fbb854
Lax Mariappan <elaxmariappan@gmail.com>
2022-05-23 19:14:26 +0530

    updated remix config

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,9 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "vercel",
   ignoredRouteFiles: ["**/.*"],
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
